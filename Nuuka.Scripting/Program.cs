fa6ceec3dea0d8b3c1ca9e560e3af450a64e76c9
Dmitrii Savchenko <dmitrii.savchenko@nuuka.com>
2022-01-28 14:17:14 +0000

    asd

---
+++ Nuuka.Scripting/Program.cs
@@ -0,0 +1,25 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using Nuuka.Scripting;
+
+var engine = Python.CreateEngine(); // Extract Python language engine from their grasp
+ScriptSource source = engine.CreateScriptSourceFromString("res = 1 + 2 + value + tag.ID + tag.First() + tag.Last()", SourceCodeKind.File);
+
+ScriptScope scope = engine.CreateScope();
+scope.SetVariable("value", 3);
+scope.SetVariable("tag", new Tag()
+{
+    ID = 1,
+    History = new List<HistoryItem>()
+    {
+        new HistoryItem(1),
+        new HistoryItem(2),
+        new HistoryItem(3)
+    }
+});
+source.Execute(scope);
+
+var res = scope.GetVariable<double>("res");
+
+Console.WriteLine(res);
---
+++ Nuuka.Scripting/Tag.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuuka.Scripting
+{   public class Tag
+    {
+        public long ID { get; set; }
+        public List<HistoryItem> History { get; set; }
+
+        public double First()
+        {
+            return this.History.FirstOrDefault().Value;
+        }
+
+        public double Last()
+        {
+            return this.History.LastOrDefault().Value;
+        }
+    }
+
+    public class HistoryItem
+    {
+        public HistoryItem(double value)
+        {
+            this.Value = value;
+        }
+
+        public double Value { get; set; }
+    }
+}
